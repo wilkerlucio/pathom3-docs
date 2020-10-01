0ce6eda0883729c8a0e48458f9e5fe4e9282d6ee
Wilker Lucio <wilkerlucio@gmail.com>
2020-10-01 14:57:55 -0300

    Add using smart maps section

--- src/cljs/com/wsscode/pathom/docs/smart_maps_demo.cljs
+++ src/cljs/com/wsscode/pathom/docs/smart_maps_demo.cljs
@@ -8,8 +8,46 @@
 
 (def indexes (pci/register full-name))
 
-(def person-data {::first-name "David" ::last-name "Nolan"})
+(def person-data {::first-name "Anne" ::last-name "Frank"})
 
 (def smart-map (psm/smart-map indexes person-data))
 
-(::full-name smart-map) ; => "David Nolan"
+(::full-name smart-map) ; => "Anne Frank"
+
+;; demo 2
+
+(pco/defresolver full-name [{::keys [first-name last-name]}]
+  ::full-name (str first-name " " last-name))
+
+(pco/defresolver anne []
+  ::anne {::first-name "Anne" ::last-name "Frank"})
+
+(def indexes (pci/register [full-name anne]))
+
+(def smart-map (psm/smart-map indexes))
+
+(::anne smart-map) ; => {::first-name "Anne" ::last-name "Frank"}
+
+; nested access
+(-> smart-map ::anne ::full-name) ; => "Anne Frank"
+
+;; demo 3
+
+(pco/defresolver full-name [{::keys [first-name last-name]}]
+  ::full-name (str first-name " " last-name))
+
+(pco/defresolver stars []
+  ::start-wars-characters
+  [{::first-name "Luke" ::last-name "Skywalker"}
+   {::first-name "Darth" ::last-name "Vader"}
+   {::first-name "Han" ::last-name "Solo"}])
+
+(def indexes (pci/register [full-name stars]))
+
+(def smart-map (psm/smart-map indexes))
+
+; nested access on sequences
+(mapv ::full-name (::start-wars-characters smart-map))
+; => ["Luke Skywalker"
+;     "Darth Vader"
+;     "Han Solo"]
