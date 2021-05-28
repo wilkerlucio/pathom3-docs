fe73302d728a152c1b01ff19ff1f73d1ed6a7657
Wilker Lucio <wilkerlucio@gmail.com>
2021-05-28 00:21:23 -0300

    Add multiple errors section

--- src/main/com/wsscode/pathom3/docs/demos/core/error_handling.clj
+++ src/main/com/wsscode/pathom3/docs/demos/core/error_handling.clj
@@ -50,22 +50,22 @@
                  [:error-demo])]
   (p.error/attribute-error response :error-demo))
 ; =>
-{:com.wsscode.pathom3.error/error-type
- :com.wsscode.pathom3.error/node-errors,
+'{:com.wsscode.pathom3.error/error-type
+  :com.wsscode.pathom3.error/node-errors,
 
- :com.wsscode.pathom3.error/node-error-details
- {1
-  {:com.wsscode.pathom3.error/error-type
-   :com.wsscode.pathom3.error/node-exception,
+  :com.wsscode.pathom3.error/node-error-details
+  {1
+   {:com.wsscode.pathom3.error/error-type
+    :com.wsscode.pathom3.error/node-exception,
 
-   :com.wsscode.pathom3.error/exception
-   #error{:cause "Example Error"
-          :data  {}
-          :via   [{:type    clojure.lang.ExceptionInfo
-                   :message "Example Error"
-                   :data    {}
-                   :at      [...]}]
-          :trace ...}}}}
+    :com.wsscode.pathom3.error/exception
+    #error{:cause "Example Error"
+           :data  {}
+           :via   [{:type    clojure.lang.ExceptionInfo
+                    :message "Example Error"
+                    :data    {}
+                    :at      [...]}]
+           :trace ...}}}}
 
 ;; output missing
 
@@ -96,32 +96,91 @@
                  [:dep-on-error])]
   (p.error/attribute-error response :dep-on-error))
 ; =>
-{:com.wsscode.pathom3.error/error-type
- :com.wsscode.pathom3.error/node-errors,
+'{:com.wsscode.pathom3.error/error-type
+  :com.wsscode.pathom3.error/node-errors,
 
- :com.wsscode.pathom3.error/node-error-details
- {1
-  {:com.wsscode.pathom3.error/error-type
-   :com.wsscode.pathom3.error/ancestor-error,
+  :com.wsscode.pathom3.error/node-error-details
+  {1
+   {:com.wsscode.pathom3.error/error-type
+    :com.wsscode.pathom3.error/ancestor-error,
 
-   :com.wsscode.pathom3.error/error-ancestor-id
-   2,
+    :com.wsscode.pathom3.error/error-ancestor-id
+    2,
 
-   :com.wsscode.pathom3.error/exception
-   #error {
-           :cause "Example Error"
-           :data  {}
-           :via   [{:type    clojure.lang.ExceptionInfo
-                    :message "Example Error"
-                    :data    {}
-                    :at      [...]}]
-           :trace [...]}}}}
+    :com.wsscode.pathom3.error/exception
+    #error {
+            :cause "Example Error"
+            :data  {}
+            :via   [{:type    clojure.lang.ExceptionInfo
+                     :message "Example Error"
+                     :data    {}
+                     :at      [...]}]
+            :trace [...]}}}}
 
 (comment
   (meta
-   (p.eql/process
-     (pci/register
-       [(pbir/constantly-fn-resolver :error-demo
-          (fn [_] (throw (ex-info "Example Error" {}))))
-        (pbir/single-attr-resolver :error-demo :dep-on-error str)])
-     [:dep-on-error])))
+    (p.eql/process
+      (pci/register
+        [(pbir/constantly-fn-resolver :error-demo
+           (fn [_] (throw (ex-info "Example Error" {}))))
+         (pbir/single-attr-resolver :error-demo :dep-on-error str)])
+      [:dep-on-error])))
+
+
+(let [response (p.eql/process
+                 (pci/register
+                   [(pco/resolver 'err1
+                      {::pco/output [:error-demo]}
+                      (fn [_ _] (throw (ex-info "One Error" {}))))
+                    (pco/resolver 'err2
+                      {::pco/output [:error-demo]}
+                      (fn [_ _] (throw (ex-info "Other Error" {}))))])
+                 [:error-demo])]
+  (p.error/attribute-error response :error-demo))
+; =>
+'{:com.wsscode.pathom3.error/error-type
+  :com.wsscode.pathom3.error/node-errors,
+
+  :com.wsscode.pathom3.error/node-error-details
+  {1
+   {:com.wsscode.pathom3.error/error-type
+    :com.wsscode.pathom3.error/node-exception,
+
+    :com.wsscode.pathom3.error/exception
+    #error {
+            :cause "Other Error"
+            :data  {}
+            :via   [{:type    clojure.lang.ExceptionInfo
+                     :message "Other Error"
+                     :data    {}
+                     :at      [...]}]
+            :trace
+                   [...]}},
+   2
+   {:com.wsscode.pathom3.error/error-type
+    :com.wsscode.pathom3.error/node-exception,
+
+    :com.wsscode.pathom3.error/exception
+    #error {
+            :cause "One Error"
+            :data  {}
+            :via   [{:type    clojure.lang.ExceptionInfo
+                     :message "One Error"
+                     :data    {}
+                     :at      [...]}]
+            :trace [...]}}}}
+
+(comment
+  (spit "static/viz/error-handling/multiple-errors.edn"
+    (with-out-str
+      (clojure.pprint/pprint
+        (meta
+          (p.eql/process
+            (pci/register
+              [(pco/resolver 'err1
+                 {::pco/output [:error-demo]}
+                 (fn [_ _] (throw (ex-info "One Error" {}))))
+               (pco/resolver 'err2
+                 {::pco/output [:error-demo]}
+                 (fn [_ _] (throw (ex-info "Other Error" {}))))])
+            [:error-demo]))))))
